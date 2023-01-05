e0b4097865b8a89ffa61bffc21963da178128ada
yesomsom <1cando1t1@naver.com>
2023-01-05 22:58:03 +0900

    add header design

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,16 +1,40 @@
 import React from "react";
 import iconPlus from '../img/icon_plus_75.png';
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+import blueOcean from '../img/blue_ocean.jpg';
 
 export default function Header() {
   return (
-    <>
-      <div>
-        <h1>BLUE</h1>
-      </div>
-      <Link to="/project_create">
-      <img className="icon_plus" src={ iconPlus } alt="create new project"/>
-      </Link>
-    </>
+    <HeaderWrap>
+      <HeaderTitle>
+        <Link to="/">BLUE</Link>
+      </HeaderTitle>
+      <MakeProject>
+        <Link to="/project_create">
+        <img className="icon_plus" src={ iconPlus } alt="create new project"/>
+        </Link>
+      </MakeProject>
+    </HeaderWrap>
   )
 }
+
+// npm install styled-components
+
+const HeaderWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: #7f87961c;
+`;
+
+const HeaderTitle = styled.div`
+  font-size: 30px;
+  padding: 10px;
+  position: relative;
+  top: 5px;
+  left: 10px;
+`;
+
+const MakeProject = styled.div`
+  padding: 10px;
+`;
