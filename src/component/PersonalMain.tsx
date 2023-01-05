b5b52ee37413b0d36e7e4062869f269364a4bfb5
yesomsom <1cando1t1@naver.com>
2023-01-06 02:47:14 +0900

    personal main page

--- src/component/PersonalMain.tsx
+++ src/component/PersonalMain.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import useFetch from '../hook/useFetch.ts';
 import Header from './Header.tsx';
 import ProjectDetails from "./ProjectDetails.tsx";
+import styled from "styled-components";
 
 export interface IProject {
   id: number;
@@ -17,11 +17,20 @@ export default function PersonalMain() {
   return (
     <>
       <Header/>
-      <div>
+      <PersonalProject>
         {projects.map(p => (
           <ProjectDetails project={p} key={p.id}/>
         ))}
-      </div>
+      </PersonalProject>
     </>
   )
 }
+
+const PersonalProject = styled.div`
+  width: 100%;
+  margin: 80px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+
