ac6dcabb291f90f1aa841b64f3a709f89fe843b1
yesomsom <1cando1t1@naver.com>
2023-01-06 15:55:52 +0900

    create project page

--- src/component/ProjectCreate.tsx
+++ src/component/ProjectCreate.tsx
@@ -6,6 +6,9 @@ import smiling from '../img/icon_smiling_96.png';
 import crying from '../img/icon_crying_96.png';
 import frowning from '../img/icon_frowning_96.png';
 import pensive from '../img/icon_pensive_96.png';
+import imgWave from '../img/img_maldives_1920.jpg';
+import Header from './Header.tsx';
+import styled from "styled-components";
 
 // 오늘 날짜 설정
 // 형식 YYYY-MM-DD
@@ -85,36 +88,60 @@ export default function ProjectCreate() {
   }
   
   return (
-    <form onSubmit={onSubmit}>
-      <div>
-        <label>제목</label>
-        <input ref={titleRef} type="text" placeholder="title"></input>
-      </div>
+    <>
+      <Header/>
+      <Layout>
+        <CreateProjectWrapper onSubmit={onSubmit}>
+          <div>
+            <label>제목</label>
+            <input ref={titleRef} type="text" placeholder="title"></input>
+          </div>
 
-      <div>
-        <label>오늘의 기분</label>
-        <div>
-          <button type="button" onClick={bigSmileFunc}><img src={bigSmiling}/></button>
-          <button type="button" onClick={smilingFunc}><img src={smiling}/></button>
-          <button type="button" onClick={cryingFunc}><img src={crying}/></button>
-          <button type="button" onClick={frowningFunc}><img src={frowning}/></button>
-          <button type="button" onClick={pensiveFunc}><img src={pensive}/></button>
-        </div>
-        <input ref={feelingRef} type="hidden" value={chooseIcon}></input>
-      </div>
+          <div>
+            <label>오늘의 기분</label>
+            <div>
+              <button type="button" onClick={bigSmileFunc}><img src={bigSmiling}/></button>
+              <button type="button" onClick={smilingFunc}><img src={smiling}/></button>
+              <button type="button" onClick={cryingFunc}><img src={crying}/></button>
+              <button type="button" onClick={frowningFunc}><img src={frowning}/></button>
+              <button type="button" onClick={pensiveFunc}><img src={pensive}/></button>
+            </div>
+            <input ref={feelingRef} type="hidden" value={chooseIcon}></input>
+          </div>
 
-      <div>
-        <label>내용</label>
-        <input ref={contentRef} type="text" placeholder="content"></input>
-      </div>
-      <div>
-        <label>작성일</label>
-        <span>{today}</span>
-      </div>
-      <div>
-        <button>저장</button>
-        <Link to="/personal_main">취소</Link>       
-      </div>
-    </form>
+          <div>
+            <label>내용</label>
+            <input ref={contentRef} type="text" placeholder="content"></input>
+          </div>
+          <div>
+            <label>작성일</label>
+            <span>{today}</span>
+          </div>
+          <div>
+            <button>저장</button>
+            <Link to="/personal_main">취소</Link>       
+          </div>
+        </CreateProjectWrapper>
+      </Layout>
+    </>
   )
 }
+
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-image: url(${imgWave});
+  background-size: cover;
+  height: 150vh;
+`;
+
+const CreateProjectWrapper = styled.form`
+  border: 1px dashed #bbb;
+  width: 500px;
+  margin: 100px;
+  background-color: #dce5e8de;
+  text-align: left;
+  padding: 40px;
+`;
+
