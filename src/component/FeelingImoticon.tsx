524010312b7f887ab656e1df313d825a0a28eab9
yesomsom <1cando1t1@naver.com>
2023-01-07 00:21:49 +0900

    imoticon

--- src/component/FeelingImoticon.tsx
+++ src/component/FeelingImoticon.tsx
@@ -4,18 +4,24 @@ import smiling from '../img/icon_smiling_96.png';
 import crying from '../img/icon_crying_96.png';
 import frowning from '../img/icon_frowning_96.png';
 import pensive from '../img/icon_pensive_96.png';
+import styled from "styled-components";
 
 export default function FeelingImoticon({feel:f}) {
 
   return (
     <>
       <div>
-        {f == "bigSmiling" && <img src={bigSmiling}></img>}
-        {f == "smiling" && <img src={smiling}></img>}
-        {f == "crying" && <img src={crying}></img>}
-        {f == "frowning" && <img src={frowning}></img>}
-        {f == "pensive" && <img src={pensive}></img>}
+        {f == "bigSmiling" && <ImoticonImg src={bigSmiling}></ImoticonImg>}
+        {f == "smiling" && <ImoticonImg src={smiling}></ImoticonImg>}
+        {f == "crying" && <ImoticonImg src={crying}></ImoticonImg>}
+        {f == "frowning" && <ImoticonImg src={frowning}></ImoticonImg>}
+        {f == "pensive" && <ImoticonImg src={pensive}></ImoticonImg>}
       </div>
     </>
   )
 }
+
+const ImoticonImg = styled.img`
+  width: 80px;
+  height: 80px;
+`;
